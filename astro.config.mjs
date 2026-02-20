03b128fb81bb9ebc7aa7a6df6a7edd5006fc9bba
Gian Marco Apaza Centeno <gianmarco3056@gmail.com>
2026-02-20 01:32:42 -0500

    Revert "fix: Eliminar adaptador SSR de Node para permitir despliegue estático en Vercel y evitar error 500"
    This reverts commit 787df96c6c66fc0f09a03d544366e877dfd90d48.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+    host: '0.0.0.0'
+  }),
   vite: {
     plugins: [tailwindcss()]
   }
